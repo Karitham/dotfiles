a1a61655b06c89f0e0088a9337e2fa1101a905c6
Karitham <kar@karitham.dev>
2022-03-16 13:19:32 +0100

    add starship config

--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --output HDMI-1-0 --mode 1920x1080 --pos 1920x0
 
 i3-msg '[workspace=2]' move workspace to output right >/dev/null
 i3-msg '[workspace=5]' move workspace to output right >/dev/null
